f3d9f78c0b136a4282422783feb4cad5c611a79b
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-07 23:45:35 +0900

    feat: movie api 설계

--- netflix/src/app.controller.ts
+++ netflix/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,50 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getMovies() {
+    return [
+      {
+        id: '1',
+        name: '해리포터',
+        character: ['해리포터', '에미왓슨'],
+      },
+      {
+        id: '2',
+        name: '반지의 제왕',
+        character: ['호비트', '라우렌'],
+      },
+    ];
+  }
+
+  @Get()
+  getMovie() {
+    return {
+      id: '1',
+      name: '해리포터',
+      character: ['해리포터', '에미왓슨'],
+    };
+  }
+
+  @Post()
+  postMovie() {
+    return {
+      id: '3',
+      name: '어벤져스',
+      character: ['토니스타크', '스티브로저스'],
+    };
+  }
+
+  @Patch()
+  patchMovie() {
+    return {
+      id: '3',
+      name: '어벤져스',
+      character: ['토니스타크', '블랙위도우'],
+    };
+  }
+
+  @Delete()
+  deleteMovie() {
+    return 3;
   }
 }
