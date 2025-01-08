4eacf73706b3143aa4d34a0f17f4a1859fc1a116
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-08 22:23:31 +0900

    chore: class validator,transformer 설치 및 세팅

--- netflix/src/main.ts
+++ netflix/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // class-validator 사용하기 위해 필요
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
