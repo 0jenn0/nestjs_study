47929ff4c892d487afe6fc984652abe26958a71e
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-08 09:41:57 +0900

    feat: get movie 구현

--- netflix/src/app.controller.ts
+++ netflix/src/app.controller.ts
@@ -1,24 +1,24 @@
 import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Movie {
+  id: string;
+  title: string;
+}
+
 @Controller('movie')
 export class AppController {
+  private movies: Movie[] = [
+    { id: '1', title: '해리포터' },
+    { id: '2', title: '반지의 제왕' },
+    { id: '3', title: '어벤져스' },
+  ];
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getMovies() {
-    return [
-      {
-        id: '1',
-        name: '해리포터',
-        character: ['해리포터', '에미왓슨'],
-      },
-      {
-        id: '2',
-        name: '반지의 제왕',
-        character: ['호비트', '라우렌'],
-      },
-    ];
+    return this.movies;
   }
 
   @Get(':id')
