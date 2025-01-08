709c7449c5e870cc93b9cbda4955eb18db075746
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-08 20:30:53 +0900

    feat: Query Parameter 사용해보기

--- netflix/src/app.controller.ts
+++ netflix/src/app.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -25,8 +26,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getMovies() {
-    return this.movies;
+  getMovies(@Query('title') title?: string) {
+    if (!title) return this.movies;
+
+    return this.movies.filter((movie) => movie.title.startsWith(title));
   }
 
   @Get(':id')
