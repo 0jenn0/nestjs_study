e87b842575f7a22066a1e8159f212a5466e5f0a0
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-08 22:34:27 +0900

    feat: 기본 validator 정리

--- netflix/src/movie/dto/update-movie.dto.ts
+++ netflix/src/movie/dto/update-movie.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { Equals, IsDefined, IsEmpty, IsIn, IsNotEmpty, IsNotIn, IsOptional, NotEquals } from 'class-validator';
 
 export class UpdateMovieDto {
   @IsNotEmpty()
@@ -8,4 +8,15 @@ export class UpdateMovieDto {
   @IsNotEmpty()
   @IsOptional()
   genre?: string;
+
+  @IsDefined() // null 이나 undefined가 아닐 때 통과
+  @IsOptional()
+  @Equals('test') // 'test' 일 때만 통과
+  @NotEquals('test') // 'test'가 아닐 때만 통과
+  @IsEmpty() // null || undefined || '' 일 때 통과
+  @IsNotEmpty() // null || undefined || '' 가 아닐 때 통과
+  // Array
+  @IsIn(['액션', '판타지']) // 배열 안에 있는 값일 때 통과
+  @IsNotIn(['액션', '판타지']) // 배열 안에 있는 값이 아닐 때 통과
+  test: string;
 }
