34e162156662860670e0b9e5c66e8a60a0d16539
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-08 21:22:44 +0900

    feat: movie 모듈로 엔드포인트 이전하기

--- netflix/src/movie/movie.service.ts
+++ netflix/src/movie/movie.service.ts
@@ -1,26 +1,63 @@
-import { Injectable } from '@nestjs/common';
-import { CreateMovieDto } from './dto/create-movie.dto';
-import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
+export interface Movie {
+  id: number;
+  title: string;
+}
 @Injectable()
 export class MovieService {
-  create(createMovieDto: CreateMovieDto) {
-    return 'This action adds a new movie';
+  private movies: Movie[] = [
+    { id: 1, title: '해리포터' },
+    { id: 2, title: '반지의 제왕' },
+  ];
+
+  getManyMovies(title?: string): Movie[] {
+    if (!title) return this.movies;
+
+    return this.movies.filter((movie) => movie.title.startsWith(title));
   }
 
-  findAll() {
-    return `This action returns all movie`;
+  getMovieById(id: number): Movie {
+    const movie = this.movies.find((movie) => movie.id === id);
+
+    if (!movie) {
+      throw new NotFoundException(`ID ${id} 영화가 없습니다.`); // 에러 메시지와 함께 404 에러 뜸
+    }
+
+    return movie;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} movie`;
+  createMovie(title: string): Movie {
+    const movie: Movie = {
+      id: this.movies.length + 1,
+      title,
+    };
+
+    this.movies.push(movie);
+
+    return movie; // 왜 return movie 하는가? 클라이언트에서도 id를 알 수 있도록 하기 위해서
   }
 
-  update(id: number, updateMovieDto: UpdateMovieDto) {
-    return `This action updates a #${id} movie`;
+  updateMovie(id: number, title: string): Movie {
+    const movie = this.movies.find((movie) => movie.id === id);
+
+    if (!movie) {
+      throw new NotFoundException(`ID ${id} 영화가 없습니다.`);
+    }
+
+    Object.assign(movie, { title });
+
+    return movie;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} movie`;
+  deleteMovie(id: number): number {
+    const movieIndex = this.movies.findIndex((movie) => movie.id === id);
+
+    if (movieIndex === -1) {
+      throw new NotFoundException(`ID ${id} 영화가 없습니다.`);
+    }
+    this.movies.splice(movieIndex, 1);
+
+    return id;
   }
 }
