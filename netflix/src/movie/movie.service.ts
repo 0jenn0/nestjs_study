6806d2f78ae42f32da6e22114998a819a6f45cc3
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-08 22:09:07 +0900

    feat: dto 생성 및 적용

--- netflix/src/movie/movie.service.ts
+++ netflix/src/movie/movie.service.ts
@@ -1,14 +1,17 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 export interface Movie {
   id: number;
   title: string;
+  genre: string;
 }
 @Injectable()
 export class MovieService {
   private movies: Movie[] = [
-    { id: 1, title: '해리포터' },
-    { id: 2, title: '반지의 제왕' },
+    { id: 1, title: '해리포터', genre: '판타지' },
+    { id: 2, title: '반지의 제왕', genre: '판타지' },
   ];
 
   getManyMovies(title?: string): Movie[] {
@@ -27,10 +30,10 @@ export class MovieService {
     return movie;
   }
 
-  createMovie(title: string): Movie {
+  createMovie(createMovieDto: CreateMovieDto): Movie {
     const movie: Movie = {
       id: this.movies.length + 1,
-      title,
+      ...createMovieDto,
     };
 
     this.movies.push(movie);
@@ -38,14 +41,14 @@ export class MovieService {
     return movie; // 왜 return movie 하는가? 클라이언트에서도 id를 알 수 있도록 하기 위해서
   }
 
-  updateMovie(id: number, title: string): Movie {
+  updateMovie(id: number, updateMovieDto: UpdateMovieDto): Movie {
     const movie = this.movies.find((movie) => movie.id === id);
 
     if (!movie) {
       throw new NotFoundException(`ID ${id} 영화가 없습니다.`);
     }
 
-    Object.assign(movie, { title });
+    Object.assign(movie, updateMovieDto);
 
     return movie;
   }
