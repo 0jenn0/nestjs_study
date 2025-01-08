565696a42973a0687156f6720546d45a8ac965fa
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-08 21:03:19 +0900

    feat: method들에 대한 로직을 service로 이전

--- netflix/src/app.controller.ts
+++ netflix/src/app.controller.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Patch,
   Post,
@@ -11,73 +10,32 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface Movie {
-  id: number;
-  title: string;
-}
-
 @Controller('movie')
 export class AppController {
-  private movies: Movie[] = [
-    { id: 1, title: '해리포터' },
-    { id: 2, title: '반지의 제왕' },
-  ];
-
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getMovies(@Query('title') title?: string) {
-    if (!title) return this.movies;
-
-    return this.movies.filter((movie) => movie.title.startsWith(title));
+    return this.appService.getManyMovies(title);
   }
 
   @Get(':id')
   getMovie(@Param('id') id: string) {
-    const movie = this.movies.find((movie) => movie.id === +id);
-
-    if (!movie) {
-      // throw new Error(`Movie with ID ${id} not found`); // 이렇게하면 걍 500 에러 뜸
-      throw new NotFoundException(`ID ${id} 영화가 없습니다.`); // 에러 메시지와 함께 404 에러 뜸
-    }
-
-    return movie;
+    return this.appService.getMovieById(+id);
   }
 
   @Post()
   postMovie(@Body('title') title: string) {
-    const movie: Movie = {
-      id: this.movies.length + 1,
-      title,
-    };
-
-    this.movies.push(movie);
-
-    return movie; // 왜 return movie 하는가? 클라이언트에서도 id를 알 수 있도록 하기 위해서
+    return this.appService.createMovie(title);
   }
 
   @Patch(':id') // id는 절대 바뀔 일 없어야한다.
   patchMovie(@Param('id') id: string, @Body('title') title: string) {
-    const movie = this.movies.find((movie) => movie.id === +id);
-
-    if (!movie) {
-      throw new NotFoundException(`ID ${id} 영화가 없습니다.`);
-    }
-
-    Object.assign(movie, { title });
-
-    return movie;
+    return this.appService.updateMovie(+id, title);
   }
 
   @Delete(':id')
   deleteMovie(@Param('id') id: string) {
-    const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
-
-    if (movieIndex === -1) {
-      throw new NotFoundException(`ID ${id} 영화가 없습니다.`);
-    }
-    this.movies.splice(movieIndex, 1);
-
-    return id;
+    return this.appService.deleteMovie(+id);
   }
 }
