4983a0d28c90754638b78fb402fcb45b242e63ab
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-11 15:52:37 +0900

    feat: validation pipe 주요 옵션

--- netflix/src/main.ts
+++ netflix/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // class-validator 사용하기 위해 필요
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 기본값 false, 이걸 true로 하면 데코레이터에 없는 속성은 제거. 따라서 대부분  이 값을 true로 둔다.
+      forbidNonWhitelisted: true, // 기본값 false, 이걸 true로 하면 데코레이터에 없는 속성은 에러 발생. 이 값은 false로 두는 경우 있음.
+    }),
+  ); // class-validator 사용하기 위해 필요
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
