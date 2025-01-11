a30a6ad0095aa09c79ec9ddfb264a95aa0a78874
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-11 19:31:50 +0900

    feat: Joi로 환경변수 검증 및 비동기로 DataSource 생성

--- netflix/src/app.module.ts
+++ netflix/src/app.module.ts
@@ -1,23 +1,47 @@
 import { Module } from '@nestjs/common';
 import { MovieModule } from './movie/movie.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [],
-      synchronize: true, // production에서는 true로 하면 절대 안됨. 코드와 맞게 데이터베이스를 싱크를 맞춘다는건데,
-      // 이렇게하면 production 데이터베이스가 날아갈 수 있는 여지가 있기 때문이다.
-      // production 모드에서는 보통 싱크를 migration으로 해결한다.
+    ConfigModule.forRoot({
+      isGlobal: true, // 이 모듈을 가져오는 모든 모듈에서 사용할 수 있도록 설정
+      validationSchema: Joi.object({
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_TYPE: Joi.string().valid('postgres').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
     }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: process.env.DB_TYPE as 'postgres',
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   entities: [],
+    //   synchronize: true, // production에서는 true로 하면 절대 안됨. 코드와 맞게 데이터베이스를 싱크를 맞춘다는건데,
+    //   // 이렇게하면 production 데이터베이스가 날아갈 수 있는 여지가 있기 때문이다.
+    //   // production 모드에서는 보통 싱크를 migration으로 해결한다.
+    // }),
     MovieModule,
   ],
 })
