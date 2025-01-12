32c02977ff4342a29b30095ce999a21196ce2eb8
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-12 16:56:06 +0900

    feat: Director 엔드포인트 완성하기

--- netflix/src/director/director.service.ts
+++ netflix/src/director/director.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDirectorDto } from './dto/create-director.dto';
 import { UpdateDirectorDto } from './dto/update-director.dto';
+import { Director } from './entity/director.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class DirectorService {
+  constructor(
+    @InjectRepository(Director)
+    private directorRepository: Repository<Director>,
+  ) {}
+
   create(createDirectorDto: CreateDirectorDto) {
-    return 'This action adds a new director';
+    return this.directorRepository.save(createDirectorDto);
   }
 
   findAll() {
-    return `This action returns all director`;
+    return this.directorRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} director`;
+    const director = this.directorRepository.findOne({
+      where: { id },
+      relations: ['movies'],
+    });
+
+    if (!director) {
+      throw new NotFoundException(`ID ${id} 감독이 없습니다.`);
+    }
+
+    return director;
   }
 
-  update(id: number, updateDirectorDto: UpdateDirectorDto) {
-    return `This action updates a #${id} director`;
+  async update(id: number, updateDirectorDto: UpdateDirectorDto) {
+    const director = await this.directorRepository.findOne({
+      where: { id },
+      relations: ['movies'],
+    });
+
+    if (!director) {
+      throw new NotFoundException(`ID ${id} 감독이 없습니다.`);
+    }
+
+    await this.directorRepository.update(id, updateDirectorDto);
+
+    const newDirector = await this.directorRepository.findOne({
+      where: { id },
+    });
+
+    return newDirector;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} director`;
+  async remove(id: number) {
+    const director = await this.directorRepository.findOne({
+      where: { id },
+    });
+
+    if (!director) {
+      throw new NotFoundException(`ID ${id} 감독이 없습니다.`);
+    }
+
+    await this.directorRepository.delete(id);
+
+    return id;
   }
 }
