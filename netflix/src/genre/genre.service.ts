e7c4271b71b9ceef7257adfe8a197541b73c3df4
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-12 18:50:57 +0900

    feat: Genre 엔드포인트 작업

--- netflix/src/genre/genre.service.ts
+++ netflix/src/genre/genre.service.ts
@@ -1,26 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateGenreDto } from './dto/create-genre.dto';
 import { UpdateGenreDto } from './dto/update-genre.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Genre } from './entities/genre.entity';
 
 @Injectable()
 export class GenreService {
-  create(createGenreDto: CreateGenreDto) {
-    return 'This action adds a new genre';
+  constructor(
+    @InjectRepository(Genre)
+    private genreRepository: Repository<Genre>,
+  ) {}
+
+  async create(createGenreDto: CreateGenreDto) {
+    const genre = await this.genreRepository.save(createGenreDto);
+    return genre;
   }
 
-  findAll() {
-    return `This action returns all genre`;
+  async findAll() {
+    const genres = await this.genreRepository.find();
+    return genres;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} genre`;
+  async findOne(id: number) {
+    const genre = await this.genreRepository.findOne({
+      where: { id },
+      relations: ['movies'],
+    });
+
+    if (!genre) {
+      throw new NotFoundException(`id ${id} 장르를 찾을 수 없습니다.`);
+    }
+
+    return genre;
   }
 
-  update(id: number, updateGenreDto: UpdateGenreDto) {
-    return `This action updates a #${id} genre`;
+  async update(id: number, updateGenreDto: UpdateGenreDto) {
+    const genre = await this.genreRepository.findOne({
+      where: { id },
+    });
+
+    if (!genre) {
+      throw new NotFoundException(`id ${id} 장르를 찾을 수 없습니다.`);
+    }
+
+    await this.genreRepository.update(id, updateGenreDto);
+
+    const newGenre = await this.genreRepository.findOne({
+      where: { id },
+    });
+
+    return newGenre;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} genre`;
+  async remove(id: number) {
+    const genre = await this.genreRepository.findOne({ where: { id } });
+
+    if (!genre) {
+      throw new NotFoundException(`id ${id} 장르를 찾을 수 없습니다.`);
+    }
+
+    await this.genreRepository.delete(id);
+
+    return id;
   }
 }
