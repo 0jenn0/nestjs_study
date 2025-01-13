b436d9220e2bf1cfafbef3c65f79803def92077e
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-14 00:22:40 +0900

    feat: User Module 작업

---
+++ netflix/src/user/user.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.userRepository.save(createUserDto);
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number) {
+    const user = this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`id ${id} 유저를 찾을 수 없습니다.`);
+    }
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`id ${id} 유저를 찾을 수 없습니다.`);
+    }
+
+    await this.userRepository.update(id, updateUserDto);
+
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async remove(id: number) {
+    const user = this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`id ${id} 유저를 찾을 수 없습니다.`);
+    }
+
+    await this.userRepository.delete(id);
+
+    return id;
+  }
+}
