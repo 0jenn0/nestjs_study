d93c21beb63687284a92b568f664e898f80b7386
choi river <130737187+0jenn0@users.noreply.github.com>
2025-01-13 22:18:57 +0900

    refactor: Mapped Types 적용

--- netflix/src/director/dto/update-director.dto.ts
+++ netflix/src/director/dto/update-director.dto.ts
@@ -1,23 +1,6 @@
-import {
-  IsDateString,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-} from 'class-validator';
 
-export class UpdateDirectorDto {
-  @IsNotEmpty()
-  @IsOptional()
-  @IsString()
-  name?: string;
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateDirectorDto } from './create-director.dto';
 
-  @IsNotEmpty()
-  @IsDateString()
-  @IsOptional()
-  dob?: Date;
-
-  @IsNotEmpty()
-  @IsOptional()
-  @IsString()
-  nationality?: string;
+export class UpdateDirectorDto extends PartialType(CreateDirectorDto) {}
 }
--- netflix/src/movie/dto/update-movie.dto.ts
+++ netflix/src/movie/dto/update-movie.dto.ts
@@ -1,27 +1,4 @@
-import {
-  ArrayNotEmpty,
-  IsArray,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-} from 'class-validator';
+import { CreateMovieDto } from './create-movie.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateMovieDto {
-  @IsNotEmpty()
-  @IsOptional()
-  title?: string;
-
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsNumber({}, { each: true })
-  @IsOptional()
-  genreIds?: number[];
-
-  @IsNotEmpty()
-  @IsOptional()
-  detail?: string;
-
-  @IsNotEmpty()
-  @IsOptional()
-  directorId?: number;
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
