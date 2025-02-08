f6ef88ef3bb0a138b3d7da1fd24e8b95f0399120
choi river <130737187+0jenn0@users.noreply.github.com>
2025-02-08 21:24:41 +0900

    feat: Swagger 세팅하기

--- netflix/src/main.ts
+++ netflix/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -11,6 +12,15 @@ async function bootstrap() {
   //   type: VersioningType.HEADER,
   //   header: 'version',
   // });
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Netflix API')
+    .setDescription('NestJS 강의')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(
     new ValidationPipe({
