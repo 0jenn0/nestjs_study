615cd01c7a52749f930933fc0b9026b519f32ef1
choi river <130737187+0jenn0@users.noreply.github.com>
2025-02-08 19:27:54 +0900

    feat: 잉여 파일 삭제 기능 구현하기

--- netflix/src/common/tasks.service.ts
+++ netflix/src/common/tasks.service.ts
@@ -1,12 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { readdir, unlink } from 'fs/promises';
+import { join, parse } from 'path';
 
 @Injectable()
 export class TasksService {
   constructor() {}
 
-  @Cron('* * * * * *') // 초 분 시 일 월 요일
   logEverySecond() {
     console.log('1초 마다 실행');
+  }
+
+  //   @Cron('* * * * * *') // 초 분 시 일 월 요일
+  async eraseOrphanFiles() {
+    const files = await readdir(join(process.cwd(), 'public', 'temp'));
+
+    const deleteFilesTargets = files.filter(file => {
+      const filename = parse(file).name; // 끝에 확장자를 제외한 이름이 나온다.
+
+      const split = filename.split('_');
+
+      if (split.length !== 2) return true;
+
+      try {
+        const date = +new Date(parseInt(split[split.length - 1]));
+        const aDayInMillisecond = 24 * 60 * 60 * 1_000;
+
+        const now = +new Date();
+
+        return now - date > aDayInMillisecond;
+      } catch {
+        return true;
+      }
+    });
+
+    await Promise.all(
+      deleteFilesTargets.map(async filename => {
+        unlink(join(process.cwd(), 'public', 'temp', filename));
+      }),
+    );
   }
 }
