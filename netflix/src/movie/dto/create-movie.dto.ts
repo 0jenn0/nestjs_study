b22332ce66cd2a0c0f83c0179a5eb0d2c7b96ebe
choi river <130737187+0jenn0@users.noreply.github.com>
2025-02-08 23:37:21 +0900

    feat: ApiProperty Annotaion 알아보기

--- netflix/src/movie/dto/create-movie.dto.ts
+++ netflix/src/movie/dto/create-movie.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   ArrayNotEmpty,
@@ -9,22 +10,42 @@ import {
 export class CreateMovieDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: '영화 제목',
+    example: '겨울왕국',
+  })
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: '영화 설명',
+    example: '겨울왕국은 겨울왕국이다.',
+  })
   detail: string;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({
+    description: '감독 객체 ID',
+    example: 1,
+  })
   directorId: number;
 
   @IsArray()
   @ArrayNotEmpty()
   @IsNumber({}, { each: true })
   @Type(() => Number)
+  @ApiProperty({
+    description: '장르 IDs',
+    example: [2, 3],
+  })
   genreIds: number[];
 
   @IsString()
+  @ApiProperty({
+    description: '영화 파일 이름',
+    example: 'aaa-bbb-ccc-ddd.jpg',
+  })
   movieFileName: string;
 }
--- netflix/src/movie/dto/get-movie.dto.ts
+++ netflix/src/movie/dto/get-movie.dto.ts
@@ -1,4 +1,5 @@
 import { CursorPaginationDto } from '@/common/dto/cursor-pagination.dto';
+import { ApiProperty } from '@nestjs/swagger';
 // import { PagePaginationDto } from '@/common/dto/page-pagination.dto';
 import { IsString, IsOptional } from 'class-validator';
 
@@ -6,5 +7,9 @@ import { IsString, IsOptional } from 'class-validator';
 export class GetMovieDto extends CursorPaginationDto {
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    description: '영화의 제목',
+    example: '프로메테우스',
+  })
   title?: string;
 }
