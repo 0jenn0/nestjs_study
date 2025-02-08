e3d433266f951ade89195ae0d1c74533892fc291
choi river <130737187+0jenn0@users.noreply.github.com>
2025-02-09 07:31:43 +0900

    feat: NestJS Test 세팅하기

--- netflix/src/user/user.service.spec.ts
+++ netflix/src/user/user.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { User } from './entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const mockUserRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+};
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        { provide: getRepositoryToken(User), useValue: mockUserRepository },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
