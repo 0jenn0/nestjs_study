f8b2ccab27b886b0ffdf076cf2f091f9058b4a17
choi river <130737187+0jenn0@users.noreply.github.com>
2025-02-08 23:47:03 +0900

    feat: ApiHideProperty 알아보기

--- netflix/src/common/entity/base-table.entity.ts
+++ netflix/src/common/entity/base-table.entity.ts
@@ -1,16 +1,20 @@
+import { ApiHideProperty } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 export class BaseTable {
   @CreateDateColumn()
-  @Exclude()
+  @Exclude() // 이거해도 Swagger에는 나온다.
+  @ApiHideProperty() // 얘도 추가해야 Swagger에 안나온다.
   createdAt: Date;
 
   @UpdateDateColumn()
   @Exclude()
+  @ApiHideProperty()
   updatedAt: Date;
 
   @VersionColumn()
   @Exclude()
+  @ApiHideProperty()
   version: number;
 }
