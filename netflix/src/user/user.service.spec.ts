a15bb8d8b36f37188e85a1866e710e1632418109
choi river <130737187+0jenn0@users.noreply.github.com>
2025-02-09 13:40:52 +0900

    feat: 첫 테스트 작성해 보기

--- netflix/src/user/user.service.spec.ts
+++ netflix/src/user/user.service.spec.ts
@@ -12,7 +12,7 @@ const mockUserRepository = {
 };
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,10 +22,27 @@ describe('UserService', () => {
       ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return all users', async () => {
+      const users = [
+        {
+          id: 1,
+          email: 'test@test.com',
+        },
+      ];
+      mockUserRepository.find.mockResolvedValue(users);
+
+      const result = await userService.findAll();
+
+      expect(mockUserRepository.find).toHaveBeenCalled();
+      expect(result).toEqual(users);
+    });
   });
 });
